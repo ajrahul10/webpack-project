dd5f890af19752b33d31a5de9f3d01b91eeec514
Rahul Ajmani <rahulajmani910@gmail.com>
2021-11-21 13:43:16 +0530

    add babel to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-const eggRecipe = {
+const omletteRecipe = {
     eggs: 4,
     salt: 1,
     veggies: 3,
     spices: 2
 }
 
-console.log(eggRecipe)
+const masalaOmletteRecipe = {
+    ...omletteRecipe,
+    masala: 1,
+    spices: 1
+}
+
+// async function getPosts() {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+//     const data = await res.json()
+//     return data
+// }
+
+// getPosts().then(posts => console.log(posts))
+
+console.log(omletteRecipe)
+console.log(masalaOmletteRecipe)
